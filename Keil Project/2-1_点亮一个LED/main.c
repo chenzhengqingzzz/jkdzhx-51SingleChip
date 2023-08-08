7553a6aa39962e8c04995377cee8c3d3595180cf
陈正清-win <tenchenzhengqing@qq.com>
2023-08-08 23:47:09 +0800

    点亮LED代码程序测试，已经成功点亮，笔记将在后面更新

---
+++ Keil Project/2-1_点亮一个LED/main.c
@@ -0,0 +1,21 @@
+/*
+ * @Author: czqczqzzzzzz(czq)
+ * @Email: tenchenzhengqing@qq.com
+ * @Date: 2023-08-08 23:03:05
+ * @LastEditors: 陈正清-win
+ * @LastEditTime: 2023-08-08 23:37:46
+ * @FilePath: \2-1_点亮一个LED\main.c
+ * @Description: 点亮第一个LED代码，需要F7编译，然后下载到单片机里
+ * 
+ * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved.
+ */
+#include <REGX52.H>
+
+void main()
+{
+	P2=0xFE;	//1111 1110
+	while(1)
+	{
+		
+	}
+}
