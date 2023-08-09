997427b8cfb8db537c5f6a8ea85e44cd8d4fe306
陈正清-win <tenchenzhengqing@qq.com>
2023-08-09 21:47:47 +0800

    详细理解代码的意思，通过控制寄存器来控制高低电平，修改程序变为间隔点亮

--- Keil Project/2-1_点亮一个LED/main.c
+++ Keil Project/2-1_点亮一个LED/main.c
@@ -3,19 +3,19 @@
  * @Email: tenchenzhengqing@qq.com
  * @Date: 2023-08-08 23:03:05
  * @LastEditors: 陈正清-win
- * @LastEditTime: 2023-08-08 23:37:46
+ * @LastEditTime: 2023-08-09 21:46:28
  * @FilePath: \2-1_点亮一个LED\main.c
  * @Description: 点亮第一个LED代码，需要F7编译，然后下载到单片机里
  * 
  * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved.
  */
-#include <REGX52.H>
+#include <REGX52.H> //里面包含引脚信息的头文件
+void main(){
+    P2 = 0x55; //0101 0101  转化为16进制
 
-void main()
-{
-	P2=0xFE;	//1111 1110
-	while(1)
-	{
-		
-	}
+    // 避免一直对P2口进行操作
+    while (1){
+        
+    }
+    
 }
