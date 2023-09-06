9184cb9785d4fbafeb224c0f3b0de5f2bd7bd724
陈正清-win <tenchenzhengqing@qq.com>
2023-09-06 23:21:21 +0800

    项目文件提交

---
+++ Keil Project/5-1_模块化编程/src/Delay.h
@@ -0,0 +1,20 @@
+/*
+ * @Author: czqczqzzzzzz(czq)
+ * @Email: tenchenzhengqing@qq.com
+ * @Date: 2023-09-06 23:06:24
+ * @LastEditors: 陈正清-win
+ * @LastEditTime: 2023-09-06 23:08:12
+ * @FilePath: \jkdzhx-51SingleChip\Keil Project\5-1_模块化编程\src\Delay.h
+ * @Description: 头文件，预编译定义
+ * 
+ * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved.
+ */
+
+// 预编译
+#ifndef __DELAY_H__
+#define __DELAY_H__
+
+// 一定要对函数进行声明
+void Delay(unsigned int time);
+
+#endif
